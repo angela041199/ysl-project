83165fd2f2fe86d3b67b9c4712aa58cc86baa837
Hannah <30120etta@gmail.com>
2023-12-14 15:26:21 +0800

    admin公版更新 coupon麵包屑更新

--- style/admin_dashboard.php
+++ style/admin_dashboard.php
@@ -13,7 +13,7 @@
     <div id="layoutSidenav_nav">
         <nav class="sb-sidenav accordion sb-sidenav-light shadow" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
-                <div class="nav">
+                <div class="nav">                    
                     <div class="sb-sidenav-menu-heading">Core</div>
                     <a class="nav-link" href="../style/admin_index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
