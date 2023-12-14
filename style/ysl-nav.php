df73c78fa25c5437f70770f8a61f43404b0c1ced
Hannah <30120etta@gmail.com>
2023-12-15 01:32:31 +0800

    ysl-nav藍色logo更新

--- style/ysl-nav.php
+++ style/ysl-nav.php
@@ -15,7 +15,7 @@
 
 <nav class="sb-topnav navbar navbar-expand navbg">
     <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3 text-white" id="logo" href="../seller/seller_dashboard.php"><img class="ob_fit" src="../style/img/ysl-logo-button-whiteborder.png" alt="LOGO"></a>  
+    <a class="navbar-brand ps-3 text-white" id="logo" href="../seller/seller_dashboard.php"><img class="ob_fit" src="../style/img/ysl-logo-blue-button-whiteborder.png" alt="LOGO"></a>  
     <!-- Sidebar Toggle-->
     <!-- <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
                 class="fas fa-bars"></i></button> -->
